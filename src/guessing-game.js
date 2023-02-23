786cb64e70e2167e018e44f4dd04146e88e561ca
Sapunov <ilsapunov@gmail.com>
2023-02-23 07:33:50 +0300

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,23 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.guissTry = Math.round((this.min + this.max) / 2);
+        return this.guissTry;
     }
 
     lower() {
-
+        this.max = this.guissTry;
+        return this.max;
     }
 
     greater() {
-
+        this.min = this.guissTry;
+        return this.min;
     }
 }
 
